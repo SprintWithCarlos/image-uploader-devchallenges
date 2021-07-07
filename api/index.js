f211ba09c102371926ba2af82ce08ec241aa6616
Carlos Marchena <sprintwithcarlos@pm.me>
2021-07-07 23:29:15 +0200

    Test API completed

---
+++ api/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+
+//Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors());
+
+//File System
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "public/images"),
+  filename: (req, file, cb) => {
+    // cb(null, req.body.name);
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+app.post("/upload", upload.single("file"), async (req, res, next) => {
+  try {
+    return res.status(200).json("File uploaded Successfully");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+//Error Handling
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+//Start Server
+app.listen(5001, () => {
+  console.log("Backend server is running");
+});
