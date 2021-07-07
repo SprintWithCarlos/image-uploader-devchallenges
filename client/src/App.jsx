8459a07e9c16e1b0253be3e7b84bbce177c464e8
Carlos Marchena <sprintwithcarlos@pm.me>
2021-07-07 18:05:07 +0200

    Footer completed

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "./components/footer/Footer";
+function App() {
+  return <div className="App">
+    <main>
+
+    </main>
+    <Footer/>
+
+  </div>;
+}
+
+export default App;
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import './footer.css'
+export const Footer = () => {
+    return (
+        <footer>
+            created by <a href="https://sprintwithcarlos.github.io/" target="_blank">Sprint With Carlos</a> · devChallenges.io
+        </footer>
+    )
+}
