6b71875b6c65c2ab4c6c49e6701b4f4cbee2d281
Carlos Marchena <sprintwithcarlos@pm.me>
2021-07-08 05:48:01 +0200

    deploy

--- client/src/components/footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import './footer.css'
-export const Footer = () => {
-    return (
-        <footer>
-            created by <a href="https://sprintwithcarlos.github.io/" target="_blank">Sprint With Carlos</a> · devChallenges.io
-        </footer>
-    )
-}
